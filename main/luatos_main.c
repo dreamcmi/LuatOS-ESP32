9d8676e2f40270bf0ee640690044f0b01adf5e89
Wendal Chen <wendal1985@gmail.com>
2021-04-14 20:47:50 +0800

    change: 调大lua vm/main stack/event stack的大小,尝试解决sys.wait报错的问题

--- main/luatos_main.c
+++ main/luatos_main.c
@@ -5,11 +5,21 @@
 #include "bget.h"
 #include "luat_base.h"
 
-#define LUAT_HEAP_SIZE (1024*100)
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define LUAT_HEAP_SIZE (64*1024)
 uint8_t luavm_heap[LUAT_HEAP_SIZE] = {0};
+
+
+void timer_callback(void* args){
+    printf("from LuatOS main timer\n");
+}
 
 void app_main(void)
 {
-    bpool(luavm_heap,LUAT_HEAP_SIZE);  // lua vm需要一块内存用于内部分配, 给出首地址及大小.
+    //xTimerCreate("luat_timer", 1000 / portTICK_RATE_MS, 1, NULL, timer_callback);
+    bpool(luavm_heap, LUAT_HEAP_SIZE);  // lua vm需要一块内存用于内部分配, 给出首地址及大小.
     luat_main();      // luat_main是LuatOS的主入口, 该方法通常不会返回.
 }
