2f693e0ede145ee65fe8fe1bc1a03993d747d46d
dreamcmi <1912544842@qq.com>
2021-04-17 17:07:41 +0800

    fix: 优化下判断方式

--- components/luat/port/luat_i2c_esp32.c
+++ components/luat/port/luat_i2c_esp32.c
@@ -3,16 +3,17 @@
 
 #include "driver/i2c.h"
 
-#define ACK_VAL 0x0                             /*!< I2C ack value */
-#define NACK_VAL 0x1                            /*!< I2C nack value */
-#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
-#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
-#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+#define ACK_VAL 0x0                /*!< I2C ack value */
+#define NACK_VAL 0x1               /*!< I2C nack value */
+#define ACK_CHECK_EN 0x1           /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0          /*!< I2C master will not check ack from slave */
+#define WRITE_BIT I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ   /*!< I2C master read */
 
-
-int luat_i2c_exist(int id) {
-    if (id == 1) {
+int luat_i2c_exist(int id)
+{
+    if (id == 1)
+    {
         return 1;
     }
     return 0;
@@ -20,7 +21,7 @@ int luat_i2c_exist(int id) {
 
 int luat_i2c_setup(int id, int speed, int slaveaddr)
 {
-    if (id == 1)
+    if (luat_i2c_exist(id))
     {
         i2c_config_t conf;
         conf.mode = I2C_MODE_MASTER;
@@ -41,7 +42,7 @@ int luat_i2c_setup(int id, int speed, int slaveaddr)
 
 int luat_ic2_close(int id)
 {
-    if (id == 1)
+    if (luat_i2c_exist(id))
     {
         i2c_driver_delete(I2C_NUM_1);
         return 0;
@@ -54,7 +55,7 @@ int luat_ic2_close(int id)
 
 int luat_i2c_send(int id, int addr, void *buff, size_t len)
 {
-    if (id == 1)
+    if (luat_i2c_exist(id))
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         i2c_master_start(cmd);
@@ -73,7 +74,7 @@ int luat_i2c_send(int id, int addr, void *buff, size_t len)
 
 int luat_i2c_recv(int id, int addr, void *buff, size_t len)
 {
-    if (id == 1)
+    if (luat_i2c_exist(id))
     {
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         i2c_master_start(cmd);
@@ -82,7 +83,7 @@ int luat_i2c_recv(int id, int addr, void *buff, size_t len)
         {
             i2c_master_read(cmd, buff, len - 1, ACK_VAL);
         }
-        i2c_master_read_byte(cmd, buff + len - 1, NACK_VAL);
+        i2c_master_read_byte(cmd, buff, NACK_VAL);
         i2c_master_stop(cmd);
         i2c_master_cmd_begin(I2C_NUM_1, cmd, 1000 / portTICK_RATE_MS);
         i2c_cmd_link_delete(cmd);
