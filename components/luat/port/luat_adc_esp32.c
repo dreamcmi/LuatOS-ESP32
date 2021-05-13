c5f0339e898c89a10fe0bf1f080c42c47618a2ed
dreamcmi <1912544842@qq.com>
2021-05-13 22:16:46 +0800

    fix: 改个注释,idf这个宏定义太多了

--- components/luat/port/luat_adc_esp32.c
+++ components/luat/port/luat_adc_esp32.c
@@ -12,68 +12,40 @@ int luat_adc_open(int pin, void* args)
         {
         case 32:
             adc1_config_channel_atten(ADC1_CHANNEL_4,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO32_CHANNEL,ADC_ATTEN_DB_0); /*idf两种设置模式，随便吧*/
             break;
         case 33:
             adc1_config_channel_atten(ADC1_CHANNEL_5,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO33_CHANNEL,ADC_ATTEN_DB_0);
             break;
         case 34:
             adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO34_CHANNEL,ADC_ATTEN_DB_0);
             break;
         case 35:
             adc1_config_channel_atten(ADC1_CHANNEL_7,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO35_CHANNEL,ADC_ATTEN_DB_0);
             break;
         case 36:
             adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO36_CHANNEL,ADC_ATTEN_DB_0);
             break;
         case 37:
             adc1_config_channel_atten(ADC1_CHANNEL_1,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO37_CHANNEL,ADC_ATTEN_DB_0);
             break;
         case 38:
             adc1_config_channel_atten(ADC1_CHANNEL_2,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO38_CHANNEL,ADC_ATTEN_DB_0);
             break;
         case 39:
             adc1_config_channel_atten(ADC1_CHANNEL_3,ADC_ATTEN_DB_0);
+            // adc1_config_channel_atten(ADC1_GPIO39_CHANNEL,ADC_ATTEN_DB_0);
             break;
         default:
             break;
         }
     }
-
-    /*
-    //这个宏定义能不能拼接呢 idf给了好几种设置模式
-    */
-    // if(pin>=32 && pin<=39)
-    // {
-    //     switch (pin)
-    //     {
-    //     case 32:
-            // adc1_config_channel_atten(ADC1_GPIO32_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 33:
-    //         adc1_config_channel_atten(ADC1_GPIO33_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 34:
-    //         adc1_config_channel_atten(ADC1_GPIO34_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 35:
-    //         adc1_config_channel_atten(ADC1_GPIO35_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 36:
-    //         adc1_config_channel_atten(ADC1_GPIO36_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 37:
-    //         adc1_config_channel_atten(ADC1_GPIO37_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 38:
-    //         adc1_config_channel_atten(ADC1_GPIO38_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     case 39:
-    //         adc1_config_channel_atten(ADC1_GPIO39_CHANNEL,ADC_ATTEN_DB_0);
-    //         break;
-    //     default:
-    //         break;
-    //     }
-    // }
     else
     {
         return -1;
@@ -89,7 +61,7 @@ int luat_adc_read(int pin, int* val, int* val2)
         {
         case 32:
             *val = adc1_get_raw(ADC1_CHANNEL_4);
-            // *val = adc1_get_raw(ADC1_GPIO32_CHANNEL);
+            // *val = adc1_get_raw(ADC1_GPIO32_CHANNEL); /*idf两种设置模式，随便吧*/
             *val2 = 0; //TODO
             break;
         case 33:
