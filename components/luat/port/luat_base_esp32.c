3c484fd4e890a0ef3e7328af4675271f59aa2675
dreamcmi <1912544842@qq.com>
2021-07-28 17:47:26 +0800

    add: lwip support

--- components/luat/port/luat_base_esp32.c
+++ components/luat/port/luat_base_esp32.c
@@ -37,6 +37,7 @@ static const luaL_Reg loadedlibs[] = {
     {"uart",luaopen_uart},    // UART操作
     {"pwm",luaopen_pwm},      // PWM操作
     {"wlan", luaopen_wlan},   // wlan/wifi联网操作
+    {"lwip",luaopen_lwip},    // lwip操作
     {NULL, NULL}};
 
 // 按不同的rtconfig加载不同的库函数
