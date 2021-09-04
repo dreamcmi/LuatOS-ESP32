512e68ad990adfb2e518428019ba8d8d8e629132
dreamcmi <1912544842@qq.com>
2021-09-05 02:33:25 +0800

    update: 增加ble初始化

--- main/luatos_main.c
+++ main/luatos_main.c
@@ -9,6 +9,8 @@
 #include "freertos/task.h"
 #include "freertos/timers.h"
 #include "nvs_flash.h"
+#include "esp_bt.h"
+
 #define LUAT_HEAP_SIZE (64*1024)
 uint8_t luavm_heap[LUAT_HEAP_SIZE] = {0};
 
@@ -16,6 +18,7 @@ uint8_t luavm_heap[LUAT_HEAP_SIZE] = {0};
 void app_main(void)
 {
     bpool(luavm_heap, LUAT_HEAP_SIZE);  
+
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
@@ -23,5 +26,11 @@ void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
+
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_controller_init(&bt_cfg);
+    esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    
     luat_main();     
 }
