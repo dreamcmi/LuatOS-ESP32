bc90d673492afcad67462f5e7374af3f18dc75db
dreamcmi <1912544842@qq.com>
2021-09-18 23:28:54 +0800

    add: 加上adc电压检测

--- components/luat/port/luat_adc_esp32.c
+++ components/luat/port/luat_adc_esp32.c
@@ -5,10 +5,17 @@
 #include "esp_adc_cal.h"
 #include "sdkconfig.h"
 
+esp_adc_cal_characteristics_t *adc_chars;
+uint32_t voltage,adcr;
+esp_err_t err;
+
 int luat_adc_open(int pin, void *args)
 {
-    adc1_config_width(ADC_WIDTH_12Bit);
-    esp_err_t err = adc1_config_channel_atten(pin, ADC_ATTEN_MAX);
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_characterize(ADC_UNIT_1,ADC_ATTEN_DB_11,ADC_WIDTH_BIT_12,0,adc_chars);
+
+    err = adc1_config_channel_atten(pin, ADC_ATTEN_DB_11);
     if (err == ESP_OK)
     {
         return 0;
@@ -17,8 +24,11 @@ int luat_adc_open(int pin, void *args)
 }
 
 int luat_adc_read(int pin, int *val, int *val2)
-{   
-    *val = adc1_get_raw(pin);
+{
+    adcr = adc1_get_raw(pin);
+    voltage =  esp_adc_cal_raw_to_voltage(adcr, adc_chars);
+    *val = adcr;
+    *val2 = voltage;
     return 0;
 }
 
