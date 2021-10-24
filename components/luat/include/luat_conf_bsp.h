1bb5f8a25e0a942088d7a89f1b201b02ab840b1f
dreamcmi <1912544842@qq.com>
2021-10-24 14:24:06 +0800

    fix: 改一下luat_conf_bsp.h

--- components/luat/include/luat_conf_bsp.h
+++ components/luat/include/luat_conf_bsp.h
@@ -1,5 +1,6 @@
-
 #ifndef LUAT_CONF_BSP
 #define LUAT_CONF_BSP
-
+#define LUAT_BSP_VERSION "V0001"
+// vfs启用
+// #define LUAT_USE_FS_VFS
 #endif
--- components/luat/include/luat_conf_esp32.h
+++
@@ -1,6 +0,0 @@
-/*
-BSP配置文件
-*/
-
-// vfs启用
-// #define LUAT_USE_FS_VFS
