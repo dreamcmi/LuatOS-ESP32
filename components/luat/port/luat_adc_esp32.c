7c129a62873619d72bea6272bf615d94d89abb74
dreamcmi <1912544842@qq.com>
2021-10-31 16:39:13 +0800

    add: adc端口判断

--- components/luat/port/luat_adc_esp32.c
+++ components/luat/port/luat_adc_esp32.c
@@ -7,13 +7,24 @@
 
 static esp_adc_cal_characteristics_t *adc_chars;
 
+int adc_exist(int c)
+{
+    if (c >= 0 && c < ADC1_CHANNEL_MAX)
+    {
+        return 1;
+    }
+    return 0;
+}
+
 int luat_adc_open(int pin, void *args)
 {
+    if (!adc_exist(pin))
+        return -1;
     esp_err_t err;
     adc1_config_width(ADC_WIDTH_BIT_12);
     if (adc_chars == NULL)
         adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_characterize(ADC_UNIT_1,ADC_ATTEN_DB_11,ADC_WIDTH_BIT_12,0,adc_chars);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, adc_chars);
 
     err = adc1_config_channel_atten(pin, ADC_ATTEN_DB_11);
     if (err == ESP_OK)
@@ -25,10 +36,11 @@ int luat_adc_open(int pin, void *args)
 
 int luat_adc_read(int pin, int *val, int *val2)
 {
-    
-    uint32_t voltage,adcr;
+    if (!adc_exist(pin))
+        return -1;
+    uint32_t voltage, adcr;
     adcr = adc1_get_raw(pin);
-    voltage =  esp_adc_cal_raw_to_voltage(adcr, adc_chars);
+    voltage = esp_adc_cal_raw_to_voltage(adcr, adc_chars);
     *val = adcr;
     *val2 = voltage;
     return 0;
@@ -36,5 +48,7 @@ int luat_adc_read(int pin, int *val, int *val2)
 
 int luat_adc_close(int pin)
 {
+    if (!adc_exist(pin))
+        return -1;
     return 0;
 }
