2795fa83fd7fce22b646e9f37c5b10fce2ef9126
dreamcmi <1912544842@qq.com>
2021-11-01 14:50:01 +0800

    add: esp32库 获取重启原因,随机数,chip型号

--- components/luat/port/luat_lib_esp32.c
+++ components/luat/port/luat_lib_esp32.c
@@ -22,10 +22,79 @@ static int l_esp32_getmac(lua_State *L)
 }
 
 
+/*
+获取重启原因
+@api esp32.getRstReason()
+@return int esp_reset_reason_t
+@usage
+esp32.getRstReason()
+*/
+static int l_esp32_get_rst_reason(lua_State *L)
+{
+    esp_reset_reason_t reason = esp_reset_reason();
+    lua_pushinteger(L,(int)reason);
+    return 1;
+}
+
+/*
+获取随机数
+@api esp32.random()
+@return number
+@usage
+esp32.random()
+*/
+static int l_esp32_random(lua_State *L)
+{
+    uint32_t r = esp_random();
+    lua_pushinteger(L,r);
+    return 1;
+}
+
+/*
+获取chip信息
+@api esp32.getchip()
+@return table
+@usage
+local re = esp32.getchip()F
+log.info("esp32", "chip", re['chip'])
+log.info("esp32", "features", re['features'])
+log.info("esp32", "cores", re['cores'])
+log.info("esp32", "revision", re['revision'])
+*/
+static int l_esp32_get_chip(lua_State *L)
+{
+    esp_chip_info_t info = {0};
+    esp_chip_info(&info);
+
+    lua_newtable(L);
+
+    lua_pushstring(L, "chip");
+	lua_pushinteger(L,info.model);
+	lua_settable(L, -3);  
+
+    lua_pushstring(L, "features");
+	lua_pushinteger(L,info.features);
+	lua_settable(L, -3);  
+
+    lua_pushstring(L, "cores");
+	lua_pushinteger(L,info.cores);
+	lua_settable(L, -3);  
+
+    lua_pushstring(L, "revision");
+	lua_pushinteger(L,info.revision);
+	lua_settable(L, -3);  
+
+    return 1;
+}
+
+
 #include "rotable.h"
 static const rotable_Reg reg_esp32[] =
     {
         {"getmac",l_esp32_getmac,0},
+        {"getRstReason",l_esp32_get_rst_reason,0},
+        {"random",l_esp32_random,0},
+        {"getchip",l_esp32_get_chip,0},
         {NULL, NULL, 0}};
 
 LUAMOD_API int luaopen_esp32(lua_State *L)
