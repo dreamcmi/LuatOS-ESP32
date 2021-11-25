6d1c7d53f6fcd07b2350566b2c730958b6565240
dreamcmi <1912544842@qq.com>
2021-11-25 14:45:39 +0800

    add: s3启用psram

--- main/luatos_main.c
+++ main/luatos_main.c
@@ -9,19 +9,47 @@
 #include "freertos/task.h"
 #include "freertos/timers.h"
 #include "nvs_flash.h"
-#include "esp_bt.h"
+#include "esp_log.h"
+
+#ifdef CONFIG_SPIRAM
+#include "spiram_psram.h"
+#endif
 
 #if CONFIG_IDF_TARGET_ESP32C3
-#define LUAT_HEAP_SIZE (64*1024)
+#define LUAT_HEAP_SIZE (96 * 1024)
 #elif CONFIG_IDF_TARGET_ESP32S3
-#define LUAT_HEAP_SIZE (100*1024)
+#define LUAT_HEAP_SIZE (100 * 1024)
 #endif
 uint8_t luavm_heap[LUAT_HEAP_SIZE] = {0};
 
 
 void app_main(void)
-{
-    bpool(luavm_heap, LUAT_HEAP_SIZE);  
+{    
+    #ifdef CONFIG_SPIRAM
+    psram_size_t t = psram_get_size();
+    switch (t)
+    {
+    case 0:
+        ESP_LOGW("InitPSRAM", "The chip has 16MBITS PSRAM");
+        bpool(heap_caps_malloc(1*1024*1024, MALLOC_CAP_SPIRAM), 1*1024*1024);
+        break;
+    case 1:
+        ESP_LOGW("InitPSRAM", "The chip has 32MBITS PSRAM");
+        bpool(heap_caps_malloc(3*1024*1024, MALLOC_CAP_SPIRAM), 3*1024*1024);
+        break;
+    case 2:
+        ESP_LOGW("InitPSRAM", "The chip has 64MBITS PSRAM");
+        bpool(heap_caps_malloc(6*1024*1024, MALLOC_CAP_SPIRAM), 6*1024*1024);
+        break;
+    default: // 到这里就是初始化失败了呗
+        ESP_LOGW("InitPSRAM", "DEFAULT:The chip has no PSRAM ");
+        bpool(luavm_heap, LUAT_HEAP_SIZE);
+        break;
+    }
+    #else
+    ESP_LOGE("InitPSRAM","NOT Defined");
+    bpool(luavm_heap, LUAT_HEAP_SIZE);
+    #endif
 
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
@@ -29,7 +57,7 @@ void app_main(void)
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );
-    
-    luat_main();     
+    ESP_ERROR_CHECK(ret);
+
+    luat_main();
 }
