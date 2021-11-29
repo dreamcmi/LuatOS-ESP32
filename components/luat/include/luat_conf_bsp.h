d4b63388cdeb3e0c676e77e410bfafd1007e163b
Wendal Chen <wendal1985@gmail.com>
2021-11-29 22:11:48 +0800

    add: 在conf_bsp.h中定义搜索路径

--- components/luat/include/luat_conf_bsp.h
+++ components/luat/include/luat_conf_bsp.h
@@ -1,6 +1,12 @@
 #ifndef LUAT_CONF_BSP
 #define LUAT_CONF_BSP
 #define LUAT_BSP_VERSION "V0001"
+
+#define LUAT_MODULE_SEARCH_PATH   "/spiffs/%s.luac", "/spiffs/%s.lua",\
+  "/spiffs/lua/%s.luac", "/spiffs/lua/%s.lua",\
+  "/spiffs/luadb/%s.luac", "/spiffs/luadb/%s.lua",\
+  "",
+
 // vfs启用
 // #define LUAT_USE_FS_VFS
 #endif
