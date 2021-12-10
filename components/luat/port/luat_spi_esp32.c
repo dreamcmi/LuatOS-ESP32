0b694e611dcfb47b3dfab957c112ec81c6512500
dreamcmi <1912544842@qq.com>
2021-12-10 19:35:40 +0800

    fix: 整理规范化spi接口

--- components/luat/port/luat_spi_esp32.c
+++ components/luat/port/luat_spi_esp32.c
@@ -17,7 +17,7 @@ static spi_device_handle_t spi3_handle = {0};
 
 int luat_spi_setup(luat_spi_t *spi)
 {
-    esp_err_t err = -1;
+    esp_err_t ret = -1;
     if (spi->id == 2)
     {
         spi_bus_config_t buscfg = {
@@ -34,9 +34,8 @@ int luat_spi_setup(luat_spi_t *spi)
             .quadhd_io_num = -1,
             .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE
         };
-        err = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "bus-err:%d", err);
+        ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+        ESP_ERROR_CHECK(ret);
         spi_device_interface_config_t dev_config;
         memset(&dev_config, 0, sizeof(dev_config));
         if (spi->CPHA == 0)
@@ -56,10 +55,9 @@ int luat_spi_setup(luat_spi_t *spi)
         dev_config.clock_speed_hz = spi->bandrate;
         dev_config.spics_io_num = -1; //用户自行控制cs
         dev_config.queue_size = 7;
-        err = spi_bus_add_device(SPI2_HOST, &dev_config, &spi_handle);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "device-err:%d", err);
-        if (err == ESP_OK)
+        ret = spi_bus_add_device(SPI2_HOST, &dev_config, &spi_handle);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
             return 0;
         else
             return -1;
@@ -74,9 +72,8 @@ int luat_spi_setup(luat_spi_t *spi)
             .quadwp_io_num = -1,
             .quadhd_io_num = -1,
             .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE};
-        err = spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "bus-err:%d", err);
+        ret = spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO);
+        ESP_ERROR_CHECK(ret);
         spi_device_interface_config_t dev_config;
         memset(&dev_config, 0, sizeof(dev_config));
         if (spi->CPHA == 0)
@@ -96,10 +93,9 @@ int luat_spi_setup(luat_spi_t *spi)
         dev_config.clock_speed_hz = spi->bandrate;
         dev_config.spics_io_num = -1; //用户自行控制cs
         dev_config.queue_size = 7;
-        err = spi_bus_add_device(SPI3_HOST, &dev_config, &spi3_handle);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "device-err:%d", err);
-        if (err == ESP_OK)
+        ret = spi_bus_add_device(SPI3_HOST, &dev_config, &spi3_handle);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
             return 0;
         else
             return -1;
@@ -111,22 +107,17 @@ int luat_spi_setup(luat_spi_t *spi)
 
 int luat_spi_close(int spi_id)
 {
-    esp_err_t err;
     if (spi_id == 2)
     {
-        err = spi_bus_remove_device(spi_handle);
-        ESP_ERROR_CHECK(err);
-        err = spi_bus_free(SPI2_HOST);
-        ESP_ERROR_CHECK(err);
+        ESP_ERROR_CHECK(spi_bus_remove_device(spi_handle));
+        ESP_ERROR_CHECK(spi_bus_free(SPI2_HOST));
         return 0;
     }
 #if CONFIG_IDF_TARGET_ESP32S3
     else if (spi_id == 3)
     {
-        err = spi_bus_remove_device(spi3_handle);
-        ESP_ERROR_CHECK(err);
-        err = spi_bus_free(SPI3_HOST);
-        ESP_ERROR_CHECK(err);
+        ESP_ERROR_CHECK(spi_bus_remove_device(spi3_handle));
+        ESP_ERROR_CHECK(spi_bus_free(SPI3_HOST));
         return 0;
     }
 #endif
@@ -136,6 +127,7 @@ int luat_spi_close(int spi_id)
 
 int luat_spi_transfer(int spi_id, const char *send_buf, size_t send_length, char *recv_buf, size_t recv_length)
 {
+    esp_err_t ret = -1;
     if (spi_id == 2)
     {
         spi_transaction_t t;
@@ -144,11 +136,10 @@ int luat_spi_transfer(int spi_id, const char *send_buf, size_t send_length, char
         t.rxlength = recv_length * 8;
         t.tx_buffer = send_buf;
         t.rx_buffer = recv_buf;
-        esp_err_t err = spi_device_polling_transmit(spi_handle, &t);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "trans-err:%d", err);
-        if (err == ESP_OK)
-            return 0;
+        ret = spi_device_polling_transmit(spi_handle, &t);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
+            return recv_length;
         else
             return -1;
     }
@@ -161,11 +152,10 @@ int luat_spi_transfer(int spi_id, const char *send_buf, size_t send_length, char
         t.rxlength = recv_length * 8;
         t.tx_buffer = send_buf;
         t.rx_buffer = recv_buf;
-        esp_err_t err = spi_device_polling_transmit(spi3_handle, &t);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "trans-err:%d", err);
-        if (err == ESP_OK)
-            return 0;
+        ret = spi_device_polling_transmit(spi3_handle, &t);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
+            return recv_length;
         else
             return -1;
     }
@@ -176,17 +166,17 @@ int luat_spi_transfer(int spi_id, const char *send_buf, size_t send_length, char
 
 int luat_spi_recv(int spi_id, char *recv_buf, size_t length)
 {
+    esp_err_t ret = -1;
     if (spi_id == 2)
     {
         spi_transaction_t t;
         memset(&t, 0, sizeof(t));
         t.rxlength = length * 8;
         t.rx_buffer = recv_buf;
-        esp_err_t err = spi_device_polling_transmit(spi_handle, &t);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "recv-err:%d", err);
-        if (err == ESP_OK)
-            return 0;
+        ret = spi_device_polling_transmit(spi_handle, &t);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
+            return length;
         else
             return -1;
     }
@@ -197,11 +187,10 @@ int luat_spi_recv(int spi_id, char *recv_buf, size_t length)
         memset(&t, 0, sizeof(t));
         t.length = length * 8;
         t.rx_buffer = recv_buf;
-        esp_err_t err = spi_device_polling_transmit(spi3_handle, &t);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "trans-err:%d", err);
-        if (err == ESP_OK)
-            return 0;
+        ret = spi_device_polling_transmit(spi3_handle, &t);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
+            return length;
         else
             return -1;
     }
@@ -213,7 +202,7 @@ int luat_spi_recv(int spi_id, char *recv_buf, size_t length)
 int luat_spi_send(int spi_id, const char *send_buf, size_t length)
 {
     spi_transaction_t t;
-    esp_err_t ret;
+    esp_err_t ret = -1;
     if (spi_id == 2)
     {
         memset(&t, 0, sizeof(t));
@@ -221,9 +210,8 @@ int luat_spi_send(int spi_id, const char *send_buf, size_t length)
         t.tx_buffer = send_buf;
         ret = spi_device_polling_transmit(spi_handle, &t);
         ESP_ERROR_CHECK(ret);
-        ESP_LOGD("SPI", "send-err:%d", ret);
         if (ret == ESP_OK)
-            return 0;
+            return length;
         else
             return -1;
     }
@@ -234,11 +222,10 @@ int luat_spi_send(int spi_id, const char *send_buf, size_t length)
         memset(&t, 0, sizeof(t));
         t.length = length * 8;
         t.tx_buffer = send_buf;
-        esp_err_t err = spi_device_polling_transmit(spi3_handle, &t);
-        ESP_ERROR_CHECK(err);
-        ESP_LOGD("SPI", "trans-err:%d", err);
-        if (err == ESP_OK)
-            return 0;
+        ret = spi_device_polling_transmit(spi3_handle, &t);
+        ESP_ERROR_CHECK(ret);
+        if (ret == ESP_OK)
+            return length;
         else
             return -1;
     }
@@ -248,13 +235,16 @@ int luat_spi_send(int spi_id, const char *send_buf, size_t length)
 }
 
 // luat_spi_device_t* 在lua层看到的是一个userdata
-int luat_spi_device_setup(luat_spi_device_t* spi_dev) {
-    int ret = -1;
+int luat_spi_device_setup(luat_spi_device_t *spi_dev)
+{
+    esp_err_t ret = -1;
     int bus_id = spi_dev->bus_id;
-    spi_device_handle_t* spi_device = luat_heap_malloc(sizeof(spi_device_handle_t));
-    if (spi_device == NULL) return ret;
+    spi_device_handle_t *spi_device = luat_heap_malloc(sizeof(spi_device_handle_t));
+    if (spi_device == NULL)
+        return ret;
     spi_dev->user_data = (void *)spi_device;
-    if (bus_id == 2){
+    if (bus_id == 2)
+    {
         spi_bus_config_t buscfg = {
 #if CONFIG_IDF_TARGET_ESP32C3
             .miso_io_num = 6,
@@ -267,34 +257,36 @@ int luat_spi_device_setup(luat_spi_device_t* spi_dev) {
 #endif
             .quadwp_io_num = -1,
             .quadhd_io_num = -1,
-            .max_transfer_sz = 4092*2
+            .max_transfer_sz = 4092 * 2
         };
         ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
         ESP_ERROR_CHECK(ret);
-        ESP_LOGD("SPI", "bus-err:%d", ret);
     }
 #if CONFIG_IDF_TARGET_ESP32S3
-    else if (bus_id == 3){
+    else if (bus_id == 3)
+    {
         spi_bus_config_t buscfg = {
             .miso_io_num = 8,
             .mosi_io_num = 9,
             .sclk_io_num = 10,
             .quadwp_io_num = -1,
             .quadhd_io_num = -1,
-            .max_transfer_sz = 4092*2};
+            .max_transfer_sz = 4092 * 2};
         ret = spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO);
         ESP_ERROR_CHECK(ret);
-        ESP_LOGD("SPI", "bus-err:%d", ret);
     }
 #endif
     spi_device_interface_config_t dev_config;
     memset(&dev_config, 0, sizeof(dev_config));
-    if (spi_dev->spi_config.CPHA == 0){
+    if (spi_dev->spi_config.CPHA == 0)
+    {
         if (spi_dev->spi_config.CPOL == 0)
             dev_config.mode = 0;
         if (spi_dev->spi_config.CPOL == 1)
             dev_config.mode = 1;
-    }else{
+    }
+    else
+    {
         if (spi_dev->spi_config.CPOL == 0)
             dev_config.mode = 2;
         if (spi_dev->spi_config.CPOL == 1)
@@ -309,38 +301,41 @@ int luat_spi_device_setup(luat_spi_device_t* spi_dev) {
     else if (bus_id == 3)
         ret = spi_bus_add_device(SPI3_HOST, &dev_config, spi_device);
 #endif
-    // ESP_ERROR_CHECK(ret);
-    ESP_LOGD("SPI", "device-err:%d", ret);
-    if(ret!=0)luat_heap_free(spi_device);
+    ESP_ERROR_CHECK(ret);
+    if (ret != 0)
+        luat_heap_free(spi_device);
     return ret;
 }
 
 //关闭SPI设备，成功返回0
-int luat_spi_device_close(luat_spi_device_t* spi_dev) {
-    int ret = -1;
+int luat_spi_device_close(luat_spi_device_t *spi_dev)
+{
+    esp_err_t ret = -1;
     int bus_id = spi_dev->bus_id;
-    if (bus_id == 2 || bus_id == 3){
-        ret = spi_bus_remove_device(*(spi_device_handle_t*)(spi_dev->user_data));
+    if (bus_id == 2 || bus_id == 3)
+    {
+        ret = spi_bus_remove_device(*(spi_device_handle_t *)(spi_dev->user_data));
         ESP_ERROR_CHECK(ret);
     }
-    luat_heap_free((spi_device_handle_t*)(spi_dev->user_data));
+    luat_heap_free((spi_device_handle_t *)(spi_dev->user_data));
     return ret;
 }
 
 //收发SPI数据，返回接收字节数
-int luat_spi_device_transfer(luat_spi_device_t* spi_dev, const char* send_buf, size_t send_length, char* recv_buf, size_t recv_length) {
-    int ret = -1;
+int luat_spi_device_transfer(luat_spi_device_t *spi_dev, const char *send_buf, size_t send_length, char *recv_buf, size_t recv_length)
+{
+    esp_err_t ret = -1;
     int bus_id = spi_dev->bus_id;
-    if (bus_id == 2 || bus_id == 3){
+    if (bus_id == 2 || bus_id == 3)
+    {
         spi_transaction_t t;
         memset(&t, 0, sizeof(t));
         t.length = send_length * 8;
         t.rxlength = recv_length * 8;
         t.tx_buffer = send_buf;
         t.rx_buffer = recv_buf;
-        esp_err_t ret = spi_device_polling_transmit(*(spi_device_handle_t*)(spi_dev->user_data), &t);
+        esp_err_t ret = spi_device_polling_transmit(*(spi_device_handle_t *)(spi_dev->user_data), &t);
         ESP_ERROR_CHECK(ret);
-        ESP_LOGD("SPI", "trans-err:%d", ret);
     }
     if (ret == 0)
         return recv_length;
@@ -349,17 +344,18 @@ int luat_spi_device_transfer(luat_spi_device_t* spi_dev, const char* send_buf, s
 }
 
 //收SPI数据，返回接收字节数
-int luat_spi_device_recv(luat_spi_device_t* spi_dev, char* recv_buf, size_t length) {
-    int ret = -1;
+int luat_spi_device_recv(luat_spi_device_t *spi_dev, char *recv_buf, size_t length)
+{
+    esp_err_t ret = -1;
     int bus_id = spi_dev->bus_id;
-    if (bus_id == 2 || bus_id == 3){
+    if (bus_id == 2 || bus_id == 3)
+    {
         spi_transaction_t t;
         memset(&t, 0, sizeof(t));
         t.rxlength = length * 8;
         t.rx_buffer = recv_buf;
-        esp_err_t ret = spi_device_polling_transmit(*(spi_device_handle_t*)(spi_dev->user_data), &t);
+        esp_err_t ret = spi_device_polling_transmit(*(spi_device_handle_t *)(spi_dev->user_data), &t);
         ESP_ERROR_CHECK(ret);
-        ESP_LOGD("SPI", "recv-err:%d", ret);
     }
     if (ret == 0)
         return length;
@@ -368,17 +364,18 @@ int luat_spi_device_recv(luat_spi_device_t* spi_dev, char* recv_buf, size_t leng
 }
 
 //发SPI数据，返回发送字节数
-int luat_spi_device_send(luat_spi_device_t* spi_dev, const char* send_buf, size_t length) {
-    int ret = -1;
+int luat_spi_device_send(luat_spi_device_t *spi_dev, const char *send_buf, size_t length)
+{
+    esp_err_t ret = -1;
     int bus_id = spi_dev->bus_id;
-    if (bus_id == 2 || bus_id == 3){
+    if (bus_id == 2 || bus_id == 3)
+    {
         spi_transaction_t t;
         memset(&t, 0, sizeof(t));
         t.length = length * 8;
         t.tx_buffer = send_buf;
-        ret = spi_device_polling_transmit(*(spi_device_handle_t*)(spi_dev->user_data), &t);
-        // ESP_ERROR_CHECK(ret);
-        ESP_LOGD("SPI", "send-err:%d", ret);
+        ret = spi_device_polling_transmit(*(spi_device_handle_t *)(spi_dev->user_data), &t);
+        ESP_ERROR_CHECK(ret);
     }
     if (ret == 0)
         return length;
