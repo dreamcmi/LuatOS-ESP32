7548e06af35950b9aeab46b48c8fbf5f0da7bf0c
dreamcmi <1912544842@qq.com>
2022-02-16 19:16:29 +0800

    update: 启用mcu库

--- components/luat/include/luat_conf_bsp.h
+++ components/luat/include/luat_conf_bsp.h
@@ -38,7 +38,7 @@
 #define LUAT_USE_BLE 1
 #define LUAT_USE_ESPHTTP 1
 #define LUAT_USE_ESPMQTT 1
-// #define LUAT_USE_MCU  1
+#define LUAT_USE_MCU  1
 #define LUAT_USE_RTC 1
 
 
--- components/luat/port/luat_mcu_esp32.c
+++ components/luat/port/luat_mcu_esp32.c
@@ -4,6 +4,47 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
-long luat_mcu_ticks(void) {
+#include "esp_system.h"
+// #include "esp_efuse.h"
+#include "sdkconfig.h"
+
+long luat_mcu_ticks(void)
+{
     return xTaskGetTickCount();
+}
+
+static char uuid[6] = {0};
+const char *luat_mcu_unique_id(size_t *t)
+{
+    *t = 6;
+    /* 可以从efuse读取uid,参考以下方法
+    // char *uid[128] = {0};
+    // for (size_t i = 0; i < 128; i++)
+    // {
+    //     uid[i] = esp_efuse_read_reg(EFUSE_BLK2, i);
+    // }
+    // memcpy(uuid, uid, 128);
+    */
+    uint8_t *mac = malloc(10);
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+    memcpy(uuid, mac, 6);
+    free(mac);
+
+    return (const char *)&uuid;
+}
+
+int luat_mcu_set_clk(size_t mhz)
+{
+    return 0;
+}
+
+int luat_mcu_get_clk(void)
+{
+#if CONFIG_IDF_TARGET_ESP32C3
+    return CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ;
+#elif CONFIG_IDF_TARGET_ESP32S3
+    return CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ;
+#else
+    return 0;
+#endif
 }
