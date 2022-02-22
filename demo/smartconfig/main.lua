5eb843dba0581cfb7ee6809d557dd9f286d60bf3
liulean <liulean@airm2m.com>
2022-02-22 20:22:58 +0800

    1. 解决smart config 无法发送ack 2. 解决smart config 概率性连接AP失败

--- demo/smartconfig/main.lua
+++ demo/smartconfig/main.lua
@@ -5,16 +5,17 @@ VERSION = "1.0.0"
 local sys = require "sys"
 
 sys.taskInit(
-    function()
+    function() 
         log.info("wlan", "wlan_init:", wlan.init())
         wlan.setMode(wlan.STATION)
 
         log.info("smartconfig", wlan.smartconfig())
 
         result, _ = sys.waitUntil("WLAN_STA_CONNECTED")
-        log.info("wlan", "WLAN_STA_CONNECTED", result)
-        -- 连上之后关闭smartconfig
-        log.info("smartconfig",wlan.smartconfigStop())
+        log.info("wlan", "WLAN_STA_CONNECTED RESULT", result)
+
+        -- WIFI连接上之后，还需要等段时间才能关闭smartconfig，不然smartconfig ack有可能无法发出，其实这里不用关闭，底层会自己关闭的
+        --log.info("smartconfig",wlan.smartconfigStop())
 
     end
 )
