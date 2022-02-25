a220443a946b1c44035ee95c8f0bf168a9d349b2
fantasygm <8637583+fantasygm@user.noreply.gitee.com>
2022-02-25 13:58:40 +0800

    add:实现挂载sd卡功能，并添加对应的lua demo

---
+++ components/luat/port/luat_sdmmc_esp32.c
@@ -0,0 +1,130 @@
+#include "luat_base.h"
+#include "esp_vfs_fat.h"
+#include "esp_log.h"
+#include "driver/spi_common.h"
+#include "driver/sdspi_host.h"
+#include "driver/sdmmc_host.h"
+#include "sdmmc_cmd.h"
+#include "driver/sdmmc_defs.h"
+#include "driver/sdmmc_types.h"
+
+
+#define PIN_NUM_MISO 6
+#define PIN_NUM_MOSI 4
+#define PIN_NUM_CLK  5
+
+static const char *TAG = "SDMMC";
+static size_t maxLen = 255;
+/*
+初始化SD卡与FAT文件系统
+@api sdmmc.init(type,clk(cs),cmd,d0)
+@int 模式 1=sdio,2=spi
+@int clk引脚号 spi模式下cs引脚
+@int cmd引脚号
+@int d0引脚号
+@return int   返回esp_err
+@usage
+-- 使用sdio挂载sd卡
+sdmmc.init(1,12,18,19)
+-- 使用spi挂载sd卡
+sdmmc.init(2,8,-1,-1)
+*/
+int l_sdmmc_init(lua_State *L){
+	ESP_LOGI(TAG, "Mounting FAT filesystem");
+	esp_err_t ret = ESP_FAIL;
+	int type = luaL_checkinteger(L,1);
+	int clk = luaL_checkinteger(L,2);
+	int cmd = luaL_checkinteger(L,3);
+	int d0 = luaL_checkinteger(L,4);
+	printf("\ntype: %d\n",type);
+	printf("cs: %d\n",clk);
+	// Mount path for the partition
+	sdmmc_card_t *mount_card = NULL;
+	const char *base_path = "/sdcard0";
+	ESP_LOGI(TAG, "Initializing SDMMC\n");
+	esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+			.format_if_mount_failed = false,
+			.max_files = 5,
+	};
+	sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+	switch (type)
+	{
+		case 1:
+#if CONFIG_IDF_TARGET_ESP32C3
+			ESP_LOGI(TAG, "C3 does not support SDIO");
+#else
+			ESP_LOGI(TAG, "Using SDIO Interface");
+			sdmmc_slot_config_t sdmmc_slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+			sdmmc_slot_config.clk = clk;
+			sdmmc_slot_config.cmd = cmd;
+			sdmmc_slot_config.d0 = d0;
+			sdmmc_slot_config.width = 1;
+			ESP_LOGI(TAG, "Mounting filesystem");
+			ret = esp_vfs_fat_sdmmc_mount(base_path, &host, &sdmmc_slot_config, &mount_config, &mount_card);
+#endif
+			break;
+		case 2:
+			ESP_LOGI(TAG, "Using SPI Interface");
+			host.slot = SDSPI_DEFAULT_HOST;
+			spi_bus_config_t bus_cfg = {
+					.mosi_io_num = PIN_NUM_MOSI,
+					.miso_io_num = PIN_NUM_MISO,
+					.sclk_io_num = PIN_NUM_CLK,
+					.quadwp_io_num = -1,
+					.quadhd_io_num = -1,
+					.max_transfer_sz = 4000,
+			};
+			sdspi_device_config_t sdspi_slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+			sdspi_slot_config.gpio_cs = clk;
+			sdspi_slot_config.host_id = host.slot;
+			ret = spi_bus_initialize(host.slot, &bus_cfg, SPI_DMA_CH_AUTO);
+			if (ret != ESP_OK) {
+				ESP_LOGE(TAG, "Failed to initialize bus.");
+				return ret;
+			}
+			ESP_LOGI(TAG, "Mounting filesystem");
+			ret = esp_vfs_fat_sdspi_mount(base_path, &host, &sdspi_slot_config, &mount_config, &mount_card);
+			break;
+		default:
+			ESP_LOGI(TAG, "sdcard type error,mount fail");
+			return -1;
+	}
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount filesystem. ");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+			              "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+		}
+		return ret;
+	}
+	ESP_LOGI(TAG, "Filesystem mounted");
+	// Card has been initialized, print its properties
+	sdmmc_card_print_info(stdout, mount_card);
+	return ret;
+}
+
+/*
+卸载sd卡文件系统
+@api sdmmc.deinit()
+@return int   返回esp_err
+@usage
+-- 卸载sd卡
+sdmmc.deinit()
+*/
+int l_sdmmc_deinit(lua_State *L){
+	return esp_vfs_fat_sdmmc_unmount();
+}
+
+#include "rotable.h"
+static const rotable_Reg reg_sdmmc[] =
+		{
+				{"init", l_sdmmc_init, 0},
+				{"deinit", l_sdmmc_deinit, 0},
+				{NULL, NULL, 0}};
+
+LUAMOD_API int luaopen_sdmmc(lua_State *L)
+{
+	luat_newlib(L, reg_sdmmc);
+	return 1;
+}
