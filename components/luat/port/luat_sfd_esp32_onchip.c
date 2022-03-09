5ee4a0694093c1a165c852773b544c75b9a6cdf0
Wendal Chen <wendal1985@gmail.com>
2022-03-09 23:57:37 +0800

    update: fdb可用了,分区表里加了一个fdb, 占用64k

--- components/luat/port/luat_sfd_esp32_onchip.c
+++ components/luat/port/luat_sfd_esp32_onchip.c
@@ -14,17 +14,10 @@
 #define LUAT_LOG_TAG "onchip"
 #include "luat_log.h"
 
-static esp_partition_t * partition;
-
+static esp_partition_t * fdb_partition;
 
 int sfd_onchip_init (void* userdata) {
-    sfd_onchip_t* onchip = (sfd_onchip_t*)userdata;
-    if (onchip == NULL)
-       return -1;
-    partition = esp_partition_find_first(0x5A, 0x5A, "script");
-    if (partition == NULL)
-        return -1;;
-    onchip->addr = (partition->address - 64 * 1024);
+    fdb_partition = esp_partition_find_first(0x5A, 0x5B, "fdb");
     return 0;
 }
 int sfd_onchip_status (void* userdata) {
@@ -32,27 +25,18 @@ int sfd_onchip_status (void* userdata) {
 }
 
 int sfd_onchip_read (void* userdata, char* buff, size_t offset, size_t len) {
-    sfd_onchip_t* onchip = (sfd_onchip_t*)userdata;
-    if (onchip == NULL)
-       return -1;
-    int ret = spi_flash_read(onchip->addr + offset, buff, len);
+    int ret = esp_partition_read(fdb_partition,  offset, buff, len);
     return ret >= 0 ? len : -1;
 }
 
 int sfd_onchip_write (void* userdata, const char* buff, size_t offset, size_t len) {
-    sfd_onchip_t* onchip = (sfd_onchip_t*)userdata;
-    if (onchip == NULL)
-       return -1;
-    int ret = spi_flash_write(onchip->addr + offset, buff, len);
+    int ret = esp_partition_write(fdb_partition, offset, buff, len);
     return ret >= 0 ? len : -1;
 }
 
 int sfd_onchip_erase (void* userdata, size_t offset, size_t len) {
-    sfd_onchip_t* onchip = (sfd_onchip_t*)userdata;
-    if (onchip == NULL)
-       return -1;
-    int ret = spi_flash_erase_range(onchip->addr + offset, len);
-    return ret;
+    int ret = esp_partition_erase_range(fdb_partition, offset, len);
+    return ret >= 0 ? len : -1;
 }
 
 int sfd_onchip_ioctl (void* userdata, size_t cmd, void* buff) {
