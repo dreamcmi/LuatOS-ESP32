cc3e732b64d739eb9098404914670db13348cb22
dreamcmi <1912544842@qq.com>
2022-03-20 00:20:49 +0800

    revert: 误开启ssl

--- components/luat/include/luat_conf_bsp.h
+++ components/luat/include/luat_conf_bsp.h
@@ -47,7 +47,7 @@
 #define LUAT_USE_ESPMQTT 1
 #define LUAT_USE_MCU  1
 #define LUAT_USE_RTC 1
-#define ESPHTTP_USE_SSL 1
+// #define ESPHTTP_USE_SSL 1
 
 //----------------------------
 // 常用工具库, 按需启用, cjson和pack是强烈推荐启用的
