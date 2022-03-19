ab638fb4077a4c47db663ff370636e24e6381e38
Wendal Chen <wendal1985@gmail.com>
2022-03-20 01:53:01 +0800

    change: 拆解cmux和shell为单独的component

--- components/luat/port/luat_log_esp32.c
+++ components/luat/port/luat_log_esp32.c
@@ -11,6 +11,8 @@
 #include "luat_cmux.h"
 #include "luat_conf_bsp.h"
 
+extern luat_cmux_t cmux_ctx;
+
 static uint8_t luat_log_uart_port = 0;
 static uint8_t luat_log_level_cur = LUAT_LOG_DEBUG;
 
@@ -23,7 +25,7 @@ void luat_log_set_uart_port(int port) {
 
 void luat_nprint(char *s, size_t l) {
 #ifdef LUAT_USE_SHELL
-    if (cmux_state == 1 && cmux_log_state ==1){
+    if (cmux_ctx.state == 1 && cmux_ctx.log_state ==1){
         luat_cmux_write(LUAT_CMUX_CH_LOG,  CMUX_FRAME_UIH & ~ CMUX_CONTROL_PF,s, l);
     }else
 #endif
@@ -32,7 +34,7 @@ void luat_nprint(char *s, size_t l) {
 
 void luat_log_write(char *s, size_t l) {
 #ifdef LUAT_USE_SHELL
-    if (cmux_state == 1 && cmux_log_state ==1){
+    if (cmux_ctx.state == 1 && cmux_ctx.log_state ==1){
         luat_cmux_write(LUAT_CMUX_CH_LOG,  CMUX_FRAME_UIH & ~ CMUX_CONTROL_PF,s, l);
     }else
 #endif
