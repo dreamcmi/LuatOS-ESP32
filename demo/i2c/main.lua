46b0954a178410bee94724e79528229392df1a9b
dreamcmi <1912544842@qq.com>
2022-04-01 22:14:00 +0800

    fix: 尝试修复iic,shtc3读取成功

--- demo/i2c/main.lua
+++ demo/i2c/main.lua
@@ -4,31 +4,30 @@ VERSION = "1.0.0"
 -- sys库是标配
 local sys = require "sys"
 
---1010 000x
 --7bit地址，不包含最后一位读写位
-local addr = 0x50
+local addr = 0x70
 -- 按照实际芯片更改编号哦
 local i2cid = 0
 
-sys.taskInit(function()
-    log.info("i2c initial",i2c.setup(0))
-    while true do
-        --第一种方式
-        i2c.send(i2cid, addr, string.char(0x01).."1234abcd")
-        sys.wait(100)
-        i2c.send(i2cid, addr, string.char(0x01))
-        local data = i2c.recv(i2cid, addr, 8)
-        log.info("i2c", "data1",data:toHex(),data)
-
-        --第二种方式
-        i2c.writeReg(i2cid, addr, 0x01, "abcd1234")
-        sys.wait(100)
-        local data = i2c.readReg(i2cid, addr, 0x01, 8)
-        log.info("i2c", "data2",data:toHex(),data)
-        sys.wait(1000)
+-- 示例使用shtc3
+sys.taskInit(
+    function()
+        log.info("i2c.init", i2c.setup(0))
+        i2c.send(i2cid, addr, string.char(0x38, 0x17)) -- 唤醒
+        -- i2c.send(i2cid, addr, string.char(0xef, 0xc8)) -- 读id
+        -- local data = i2c.recv(i2cid, addr, 3)
+        -- log.info("recv",data:toHex())
+        while true do
+            i2c.send(i2cid, addr, string.char(0x64, 0x58)) -- 低功耗 温度在前
+            sys.wait(100)
+            local data = i2c.recv(i2cid, addr, 6)
+            local _, tval, ccrc, hval, hcrc = pack.unpack(data, ">HbHb")
+            log.info("shtc3.tem", string.format("%.1fC", ((tval / 65536.0) * 175) - 45))
+            log.info("shtc3.hum", string.format("%.1f%%", (hval / 65536.0) * 100))
+            sys.wait(1000)
+        end
     end
-
-end)
+)
 
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
