f4cd0182692b95b8c0cb3899ad61430b390f6831
dreamcmi <1912544842@qq.com>
2022-05-07 00:01:44 +0800

    fix: smartconfig还是先用老方案做demo默认吧

--- demo/wifi/smartconfig/main.lua
+++ demo/wifi/smartconfig/main.lua
@@ -5,38 +5,38 @@ VERSION = "1.0.0"
 local sys = require "sys"
 local STA_MODE = 0
 
--- sys.taskInit(
---     function() 
---         log.info("wlan", "wlan_init:", wlan.init())
---         wlan.setMode(wlan.STATION)
-
---         log.info("smartconfig", wlan.smartconfig())
-
---         result, _ = sys.waitUntil("WLAN_STA_CONNECTED")
---         log.info("wlan", "WLAN_STA_CONNECTED RESULT", result)
-
---         -- WIFI连接上之后，还需要等段时间才能关闭smartconfig，不然smartconfig ack有可能无法发出，其实这里不用关闭，底层会自己关闭的
---     end
--- )
-
 sys.taskInit(
-    function()
+    function() 
         log.info("wlan", "wlan_init:", wlan.init())
         wlan.setMode(wlan.STATION)
 
-        log.info("smartconfig","start")
-        local r = wlan.taskSmartconfig(0,120).wait() --模式0，超时120秒
-        log.info("smartconfig", r and "connected!" or "connect fail")
+        log.info("smartconfig", wlan.smartconfig())
 
-        -- ack代表返回手机配网成功,根据自身情况选择是否开启
-        result, _ = sys.waitUntil("SMARTCONFIG_ACK_DONE")
-        log.info("wlan", "SMARTCONFIG_ACK_DONE", result)
+        result, _ = sys.waitUntil("WLAN_STA_CONNECTED")
+        log.info("wlan", "WLAN_STA_CONNECTED RESULT", result)
 
         t = wlan.getConfig(STA_MODE)
         log.info("wlan", "wifi connected info", t.ssid, t.password, t.bssid:toHex())
-        -- WIFI连接上之后，还需要等段时间才能关闭smartconfig，不然smartconfig ack有可能无法发出，其实这里不用关闭，底层会自己关闭的
     end
 )
+
+-- sys.taskInit(
+--     function()
+--         log.info("wlan", "wlan_init:", wlan.init())
+--         wlan.setMode(wlan.STATION)
+
+--         log.info("smartconfig","start")
+--         local r = wlan.taskSmartconfig(0,120).wait() --模式0，超时120秒
+--         log.info("smartconfig", r and "connected!" or "connect fail")
+
+--         -- ack代表返回手机配网成功,根据自身情况选择是否开启
+--         result, _ = sys.waitUntil("SMARTCONFIG_ACK_DONE")
+--         log.info("wlan", "SMARTCONFIG_ACK_DONE", result)
+
+--         t = wlan.getConfig(STA_MODE)
+--         log.info("wlan", "wifi connected info", t.ssid, t.password, t.bssid:toHex())
+--     end
+-- )
 
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
