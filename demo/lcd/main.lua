e46fc7b20eae10f5be6da022bb0164e5a26459a9
Wendal Chen <wendal1985@gmail.com>
2022-05-11 21:00:06 +0800

    update: 更新demo/lcd

--- demo/lcd/main.lua
+++ demo/lcd/main.lua
@@ -17,69 +17,26 @@ _G.sys = require("sys")
 
 spi_lcd = spi.deviceSetup(2, 7, 0, 0, 8, 40000000, spi.MSB, 1, 1)
 
--- lcd.init("st7789", {
---     port = "device",
---     pin_dc = 6,
---     pin_rst = 10,
---     direction = 0,
---     w = 240,
---     h = 320,
---     xoffset = 0,
---     yoffset = 0
--- }, spi_lcd)
--- log.info("lcd.init",
--- lcd.init("st7735v",{port = "device",pin_dc = 6, pin_rst = 10,direction = 0,w = 80,h = 160,xoffset = 26,yoffset = 1},spi_lcd)
--- 屏幕反色是因为驱动里有反色的命令，用户自行注释luatos主库-->lcd-->luat_lcd_st7735v.c文件中的 lcd_write_cmd(conf,0x21)然后自行编译源码
+--[[ 此为合宙售卖的2.4寸TFT LCD 分辨率:240X320 屏幕ic:GC9306 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.39.6c2275a1Pa8F9o&id=655959696358]]
+-- lcd.init("gc9a01",{port = "device",pin_dc = 6, pin_pwr = 10,pin_rst = pin.PB03,direction = 0,w = 240,h = 320,xoffset = 0,yoffset = 0},spi_lcd)
 
---下面为custom方式示例,自己传入lcd指令来实现驱动,示例以st7735v做展示
-log.info("lcd.init",
-lcd.init("custom",{
-    port = "device",
-    pin_dc = 6, 
-    pin_rst = 10,
-    w = 160,
-    h = 80,
-    xoffset = 0,
-    yoffset = 24,
-    sleepcmd = 0x10,
-    wakecmd = 0x11,
-    initcmd = {--0001 delay  0002 cmd  0003 data
-        0x00020011,0x00010078,--0x00020021, -- 反显
-        0x000200B1,0x00030002,0x00030035,
-        0x00030036,0x000200B2,0x00030002,
-        0x00030035,0x00030036,0x000200B3,
-        0x00030002,0x00030035,0x00030036,
-        0x00030002,0x00030035,0x00030036,
-        0x000200B4,0x00030007,0x000200C0,
-        0x000300A2,0x00030002,0x00030084,
-        0x000200C1,0x000300C5,0x000200C2,
-        0x0003000A,0x00030000,0x000200C3,
-        0x0003008A,0x0003002A,0x000200C4,
-        0x0003008A,0x000300EE,0x000200C5,
-        0x0003000E,0x00020036,0x00030078,
-        0x000200E0,0x00030012,0x0003001C,
-        0x00030010,0x00030018,0x00030033,
-        0x0003002C,0x00030025,0x00030028,
-        0x00030028,0x00030027,0x0003002F,
-        0x0003003C,0x00030000,0x00030003,
-        0x00030003,0x00030010,0x000200E1,
-        0x00030012,0x0003001C,0x00030010,
-        0x00030018,0x0003002D,0x00030028,
-        0x00030023,0x00030028,0x00030028,
-        0x00030026,0x0003002F,0x0003003B,
-        0x00030000,0x00030003,0x00030003,
-        0x00030010,0x0002003A,0x00030005,
-        0x00020029,
-    },
-    },
-    spi_lcd))
+--[[ 此为合宙售卖的1.8寸TFT LCD LCD 分辨率:128X160 屏幕ic:st7735 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.19.6c2275a1Pa8F9o&id=560176729178]]
+-- lcd.init("st7735",{port = "device",pin_dc = 6, pin_pwr = 10,pin_rst = pin.PB03,direction = 0,w = 128,h = 160,xoffset = 2,yoffset = 1},spi_lcd)
+
+--[[ 此为合宙售卖的1.54寸TFT LCD LCD 分辨率:240X240 屏幕ic:st7789 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.20.391445d5Ql4uJl&id=659456700222]]
+-- lcd.init("st7789",{port = "device",pin_dc = 6, pin_pwr = 10,pin_rst = pin.PB03,direction = 0,w = 240,h = 240,xoffset = 0,yoffset = 0},spi_lcd)
+
+--[[ 此为合宙售卖的0.96寸TFT LCD LCD 分辨率:160X80 屏幕ic:st7735s 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.19.6c2275a1Pa8F9o&id=560176729178]]
+lcd.init("st7735s",{port = "device",pin_dc = 6, pin_pwr = 10,pin_rst = pin.PB03,direction = 2,w = 160,h = 80,xoffset = 0,yoffset = 0},spi_lcd)
 
 
-log.info("lcd.drawLine", lcd.drawLine(20, 20, 150, 20, 0x001F))
-log.info("lcd.drawRectangle", lcd.drawRectangle(20, 40, 120, 70, 0xF800))
-log.info("lcd.drawCircle", lcd.drawCircle(50, 50, 20, 0x0CE0))
+sys.taskInit(function() 
+    sys.wait(500)
 
-sys.taskInit(function() while 1 do sys.wait(500) end end)
+    log.info("lcd.drawLine", lcd.drawLine(20, 20, 150, 20, 0x001F))
+    log.info("lcd.drawRectangle", lcd.drawRectangle(20, 40, 120, 70, 0xF800))
+    log.info("lcd.drawCircle", lcd.drawCircle(50, 50, 20, 0x0CE0))
+end)
 
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
