824c7c89c9222188b8b3a7435fd12ee18203408e
Wendal Chen <wendal1985@gmail.com>
2022-05-12 11:32:48 +0800

    update: demo/gpio作为出厂固件,添加文档链接的打印

--- demo/gpio/main.lua
+++ demo/gpio/main.lua
@@ -6,15 +6,18 @@ local sys = require "sys"
 
 sys.taskInit(
     function()
+        -- 开发板上的2个LED
         local LED_D4 = gpio.setup(12, 0)
         local LED_D5 = gpio.setup(13, 0)
         while 1 do
             LED_D4(0)
             LED_D5(1)
-            sys.wait(1000)
+            sys.wait(500)
             LED_D4(1)
             LED_D5(0)
-            sys.wait(1000)
+            sys.wait(500)
+            log.info("main", "code", "https://gitee.com/dreamcmi/LuatOS-ESP32")
+            log.info("main", "wiki", "https://wiki.luatos.com")
         end
     end
 )
