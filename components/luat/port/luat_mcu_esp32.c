020be184f2c5717f6d6cb670616783ea9a1ab18a
dreamcmi <1912544842@qq.com>
2022-05-21 19:14:35 +0800

    fix: uid获取可选mac或者UNIQUE_ID

--- components/luat/port/luat_mcu_esp32.c
+++ components/luat/port/luat_mcu_esp32.c
@@ -9,6 +9,17 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include "sdkconfig.h"
+#include "esp_pm.h"
+#include "esp_system.h"
+
+// #define UUID_USE_EFUSE
+#define UUID_USE_MAC
+
+#ifdef UUID_USE_EFUSE
+#undef UUID_USE_MAC
+#include "esp_efuse.h"
+#endif
 
 #if CONFIG_IDF_TARGET_ESP32S3
 #include "esp32s3/clk.h"
@@ -18,34 +29,32 @@
 #error "luat_mcu_esp32 target error"
 #endif
 
-#include "esp_pm.h"
-#include "esp_system.h"
-// #include "esp_efuse.h"
-#include "sdkconfig.h"
-
 long luat_mcu_ticks(void)
 {
     return xTaskGetTickCount();
 }
 
-static char uuid[6] = {0};
+static char uuid[16] = {0};
 const char *luat_mcu_unique_id(size_t *t)
 {
+#ifdef UUID_USE_EFUSE
+    *t = 16;
+    int uid[16] = {0};
+    for (size_t i = 0; i < 16; i++)
+    {
+        uid[i] = esp_efuse_read_reg(EFUSE_BLK2, i);
+    }
+    memcpy(uuid, uid, 16);
+
+#else
     *t = 6;
-    /* 可以从efuse读取uid,参考以下方法
-    // char *uid[128] = {0};
-    // for (size_t i = 0; i < 128; i++)
-    // {
-    //     uid[i] = esp_efuse_read_reg(EFUSE_BLK2, i);
-    // }
-    // memcpy(uuid, uid, 128);
-    */
     uint8_t *mac = malloc(10);
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
     memcpy(uuid, mac, 6);
     free(mac);
+#endif
 
-    return (const char *)&uuid;
+    return (const char *)uuid;
 }
 
 int luat_mcu_set_clk(size_t mhz)
