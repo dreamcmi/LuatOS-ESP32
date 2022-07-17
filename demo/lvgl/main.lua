b7bee3e5d50b64dd930bd629fe7e84d908631119
Wendal Chen <wendal1985@gmail.com>
2022-07-17 23:21:31 +0800

    update: 优化demo/lvgl

--- demo/lvgl/main.lua
+++ demo/lvgl/main.lua
@@ -14,23 +14,38 @@ _G.sys = require("sys")
 
 log.info("hello luatos")
 
-spi_lcd = spi.deviceSetup(2,5,0,0,8,40000000,spi.MSB,1,1)
-log.info("lcd.init",
-lcd.init("st7789",{port = "device",pin_dc = 7, pin_rst = 8,direction = 0,w = 240,h = 320,xoffset = 0,yoffset = 0},spi_lcd))
+spi_lcd = spi.deviceSetup(2, 7, 0, 0, 8, 40000000, spi.MSB, 1, 1)
 
-log.info("lvgl", lvgl.init())
-lvgl.disp_set_bg_color(nil, 0xFFFFFF)
-local scr = lvgl.obj_create(nil, nil)
-local btn = lvgl.btn_create(scr)
-lvgl.obj_align(btn, lvgl.scr_act(), lvgl.ALIGN_CENTER, 0, 0)
-local label = lvgl.label_create(btn)
-lvgl.label_set_text(label, "LuatOS!")
-lvgl.scr_load(scr)
+--[[ 此为合宙售卖的2.4寸TFT LCD 分辨率:240X320 屏幕ic:GC9306 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.39.6c2275a1Pa8F9o&id=655959696358]]
+-- lcd.init("gc9a01",{port = "device",pin_dc = 6, pin_pwr = 11,pin_rst = 10,direction = 0,w = 240,h = 320,xoffset = 0,yoffset = 0},spi_lcd)
+
+--[[ 此为合宙售卖的1.8寸TFT LCD LCD 分辨率:128X160 屏幕ic:st7735 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.19.6c2275a1Pa8F9o&id=560176729178]]
+-- lcd.init("st7735",{port = "device",pin_dc = 6, pin_pwr = 11,pin_rst = 10,direction = 0,w = 128,h = 160,xoffset = 2,yoffset = 1},spi_lcd)
+
+--[[ 此为合宙售卖的1.54寸TFT LCD LCD 分辨率:240X240 屏幕ic:st7789 购买地址:https://item.taobao.com/item.htm?spm=a1z10.5-c.w4002-24045920841.20.391445d5Ql4uJl&id=659456700222]]
+-- lcd.init("st7789",{port = "device",pin_dc = 6, pin_pwr = 11,pin_rst = 10,direction = 0,w = 240,h = 240,xoffset = 0,yoffset = 0},spi_lcd)
+
+--[[ 此为合宙售卖的0.96寸TFT LCD LCD 分辨率:160X80 屏幕ic:st7735s 购买地址:https://item.taobao.com/item.htm?id=661054472686]]
+lcd.init("st7735v",{port = "device",pin_dc = 6, pin_pwr = 11,pin_rst = 10,direction = 1,w = 160,h = 80,xoffset = 0,yoffset = 24},spi_lcd)
+--如果显示颜色相反，关闭反色
+lcd.invoff()
+--如果显示依旧不正常，可以尝试老版本的板子的驱动
+--lcd.init("st7735s",{port = "device",pin_dc = 6, pin_pwr = 11,pin_rst = 10,direction = 2,w = 160,h = 80, xoffset = 1,yoffset = 26},spi_lcd)
+
 
 sys.taskInit(function()
-    while 1 do
-        sys.wait(1000)
-    end
+    sys.wait(100)
+
+    log.info("lvgl", lvgl.init())
+    lvgl.disp_set_bg_color(nil, 0xFFFFFF)
+    local scr = lvgl.obj_create(nil, nil)
+    local btn = lvgl.btn_create(scr)
+    local img = lvgl.img_create(scr)
+    lvgl.obj_align(btn, lvgl.scr_act(), lvgl.ALIGN_CENTER, 0, 0)
+    local label = lvgl.label_create(btn)
+    lvgl.label_set_text(label, "LuatOS!")
+    lvgl.img_set_src(img, "/luadb/logo.jpg")
+    lvgl.scr_load(scr)
 end)
 
 
