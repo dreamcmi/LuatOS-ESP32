3aa4a1b05c4abbb2f93aa97d59e3adf0bcf3cc75
Wendal Chen <wendal1985@gmail.com>
2022-08-09 12:18:02 +0800

    add: 关联iotauth库

--- components/luat/port/luat_base_esp32.c
+++ components/luat/port/luat_base_esp32.c
@@ -215,6 +215,9 @@ static const luaL_Reg loadedlibs[] = {
 #ifdef LUAT_USE_FONTS
     {"fonts", luaopen_fonts},
 #endif
+#ifdef LUAT_USE_IOTAUTH
+    {"iotauth", luaopen_iotauth},
+#endif
     {NULL, NULL}};
 
 // 按不同的rtconfig加载不同的库函数
