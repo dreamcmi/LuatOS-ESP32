624a03fc80867b5fc725ae5d7c26c2af2d98540f
Wendal Chen <wendal1985@gmail.com>
2022-09-28 21:05:13 +0800

    update: luat_crypto_trng的实现应该没问题的,启用之

--- components/luat/port/luat_crypto_esp32.c
+++ components/luat/port/luat_crypto_esp32.c
@@ -7,9 +7,9 @@
 #include "luat_base.h"
 #include "luat_crypto.h"
 
-//#include "esp_random.h"
+#include "esp_random.h"
 int luat_crypto_trng(char *buff, size_t len)
 {
-    //esp_fill_random(buff, len);
+    esp_fill_random(buff, len);
     return 0;
 }
